b1f43f9b3c5281e58ce77e777440cb3f5a5903c5
shruthikompella <shruthikompella@users.noreply.github.com>
2020-05-11 02:30:07 -0500

    Delete forms.py

--- api/forms.py
+++
@@ -1,6 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField
-
-class RatingForm(FlaskForm):
-    review = StringField('Review')
-    predict = SubmitField('Predict')
